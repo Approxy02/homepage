d7fd5dfe48bba0a3a96a7987e1da8249cdb8f972
Approxy <klngscrown0220@gmail.com>
2024-12-25 17:11:50 +0900

    Change: Router basename

--- homepage/src/App.js
+++ homepage/src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import MainPage from './components/MainPage';
 
 const App = () => {
   return (
-    <Router>
+    <Router basename={process.env.PUBLIC_URL}>
       <Routes>
         <Route path="/" element={<MainPage />} />
       </Routes>
