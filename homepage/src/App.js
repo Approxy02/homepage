60bc694b62d1dc396d0b0af278fb936e45f9963f
Approxy <klngscrown0220@gmail.com>
2024-12-25 17:29:00 +0900

    Change: App.js Rollback

--- homepage/src/App.js
+++ homepage/src/App.js
@@ -2,21 +2,14 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import MainPage from './components/MainPage';
 
-// const App = () => {
-//   return (
-//     <Router basename={process.env.PUBLIC_URL}>
-//       <Routes>
-//         <Route path="/" element={<MainPage />} />
-//       </Routes>
-//     </Router>
-//   );
-// };
-
-// export default App;
-
 const App = () => {
   return (
-    <h1>GitHub Pages로 리액트 프로젝트 배포</h1>
+    <Router basename={process.env.PUBLIC_URL}>
+      <Routes>
+        <Route path="/" element={<MainPage />} />
+      </Routes>
+    </Router>
   );
 };
+
 export default App;
