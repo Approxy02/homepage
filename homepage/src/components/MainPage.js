c7ecb2b02547cd2d9271c917028b8f6cbc12d13e
Approxy <klngscrown0220@gmail.com>
2026-03-23 20:53:16 +0900

    변경: CV 링크의 파일 이름 수정

--- homepage/src/components/MainPage.js
+++ homepage/src/components/MainPage.js
@@ -24,7 +24,7 @@ const MainPage = () => {
             <a href="mailto:wltkdwns0220@naver.com">Email</a>
             <span className="link-sep">/</span>
             <a
-              href={`${publicUrl}/assets/[지상준]_CV).pdf`}
+              href={`${publicUrl}/assets/[지상준]_CV.pdf`}
               target="_blank"
               rel="noopener noreferrer"
             >
